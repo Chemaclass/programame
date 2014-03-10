99327ac54d5beee1b05c5e51882f58390b6e6dcc
José María Valera Reales <chemaclass@outlook.es>
2014-03-10 23:34:12 +0100

    Añadimos doMain

--- src/semaforos/Semaforos.java
+++ src/semaforos/Semaforos.java
@@ -1,12 +1,25 @@
 package semaforos;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author chemaclass
  */
 public class Semaforos {
 
+    /**
+     * Buffer de entrada
+     */
+    public static final BufferedReader BR = new BufferedReader(new InputStreamReader(System.in));
+
     public static void main(String[] args) {
-        System.out.println("");
+        new Semaforos().doMain(args);
     }
+
+    public void doMain(String... args) {
+        
+    }
+
 }
