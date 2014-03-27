7f672b436c635cbdc44c00b52d6d4cb97430341a
José María Valera Reales <chemaclass@outlook.es>
2014-03-27 14:32:20 +0100

    Creo main principal + entrada; controlamos que se introduzcan 5 valores como parámetros

--- src/pruebadel9/PruebaDel9.java
+++ src/pruebadel9/PruebaDel9.java
@@ -1,6 +1,7 @@
 package pruebadel9;
 
-import utilidades.Utils;
+import java.io.IOException;
+import static utilidades.Utils.*;
 
 /**
  *
@@ -8,8 +9,54 @@ import utilidades.Utils;
  */
 public class PruebaDel9 {
 
+    Casos casos;
+
     public static void main(String[] args) {
-        
+        new PruebaDel9().doMain(args);
     }
+
+    private void doMain(String... args) {
+        d("Introduce el número de casos de prueba");
+        try {
+            int num = Integer.parseInt(BR.readLine());
+            casos = new Casos(num);
+            //rellenamos todos los casos de prueba
+            for (int i = 0; i < num; i++) {
+                try {
+                    String input = BR.readLine();
+                    String[] inputs = input.split(" ");
+                    //Si se pasaron !=5 param, salta exception y no cuenta i
+                    if (inputs.length != 5) {
+                        throw new Exception("Número de parámetros incorrecto. Deben ser 5");
+                    }
+                    
+                    
+                } catch (Exception e) {
+                    de(e);
+                    i--; //restamos por el error
+                }
+            }
+
+        } catch (NumberFormatException | IOException e) {
+            de(e);
+        }
+        d(casos.getNumCasos());
+    }
+}
+
+class Casos {
+
+    public final Division[] CASOS_PRUEBA;
+
+    public Casos(int num) {
+        CASOS_PRUEBA = new Division[num];
+    }
+
+    public int getNumCasos() {
+        return CASOS_PRUEBA.length;
+    }
+}
+
+class Division {
 
 }
